13316c0101954040ec43940c211edf9c77ab94d3
Tijo-11 <tijot111@gmail.com>
2025-01-14 16:24:15 +0530

    Few more steps to complete receipe

--- vege/views.py
+++ vege/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import *
+from django.http import HttpResponse
 
 # Create your views here.
 def receipes(request):
@@ -17,7 +18,28 @@ def receipes(request):
             receipe_image=receipe_image)
     
         return redirect('/receipes')
-        
-    return render(request, 'receipes.html')
+    queryset=Receipe.objects.all()
+    context={'receipes':queryset}
+    return render(request, 'receipes.html',context)
 
+def delete_receipe(request, id):
+    queryset= Receipe.objects.get(id = id)
+    queryset.delete()
+    return redirect('/receipes')
+
+def update_receipe(request, id):
+    queryset= Receipe.objects.get(id = id)
+    if request.method=="POST":
+        data=request.POST
+        receipe_name=data.get('receipe_name')
+        receipe_description=data.get('receipe_description')
+        receipe_image=request.FILES.get('receipe_image')
+        queryset.receipe_name=receipe_name
+        queryset.receipe_description=receipe_description
+        queryset.receipe_image=receipe_image
+        queryset.save()
+        return redirect('/receipes')
+    context={'receipe':queryset}
+    return render(request, 'update_receipes.html',context)
+    
     
